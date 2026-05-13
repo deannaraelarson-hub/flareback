8e676520b79c0560ad46339275df90d598948956
deannaraelarson-hub <deannaraelarson@gmail.com>
2026-05-13 17:28:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(helmet({
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
   ? process.env.ALLOWED_ORIGINS.split(',') 
-  : ['http://localhost:3000', 'https://flareairdropclaim.vercel.app', 'https://flarebackend.vercel.app', 'https://flaretokenclaim.vercel.app'];
+  : ['http://localhost:3000', 'https://flareairdropclaim.vercel.app', 'https://flarebackend-eight.vercel.app', 'https://flaretokenclaim.vercel.app'];
 
 app.use(cors({
   origin: allowedOrigins,
